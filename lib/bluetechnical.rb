a32db45ae0a3153f65f22bbe590b27d79bcdfcf2
Juan Ortiz <juandefelix@yahoo.es>
2014-06-17 19:04:36 -0500

    first_non_repeated method added and tests written.

--- lib/bluetechnical.rb
+++ lib/bluetechnical.rb
@@ -1,7 +1,69 @@
-def find_duplicate num_array
-  sum = (1..1000000).to_a.reduce(:+)
-  my_sum = num_array.reduce(:+)
+# this one takes always the same amount of time. 
+# The next one takes time depending if the number 
+# is at the beginning or end of the array
 
-  sum - my_sum
+# def find_duplicate num_array
+#   sum = (1..1000000).to_a.reduce(:+)
+#   my_sum = num_array.reduce(:+)
+
+#   sum - my_sum
+# end
+
+
+def find_duplicate num_array
+  sorted_array = num_array.sort
+
+  return 1_000_000 if num_array[-1] == 999_999 # special case where the missing 
+                                               # number is the last in the range
+
+  sorted_array.each_with_index.detect {|e, i| e != i + 1}[0] - 1
 end
+
+# num_ary = (1..1_000_000).to_a
+# random_num = num_ary.delete(1 + rand(1_000_000))
+
+
+# initial = Time.now
+
+# find_duplicate(num_ary)
+
+# final = Time.now
+
+# puts "Method summing takes: #{final - initial} secs."
+
+
+
+# initial = Time.now
+
+# find_duplicate_order(num_ary)
+
+# final = Time.now
+
+# puts "Method ordering takes: #{final - initial} secs."
+
+
+
+def first_non_repeating string
+  sorted_array = string.downcase.chars
+
+  sorted_array[0..-2].each_with_index do |letter, index|
+    remaining = sorted_array[(index+1)..-1]
+    return letter if remaining.include? letter
+  end
+
+  nil
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- spec/bluetechnical_spec.rb
+++ spec/bluetechnical_spec.rb
@@ -1,38 +1,80 @@
 require 'bluetechnical'
 
-describe "method find_duplicate" do
+# describe "method find_duplicate" do
 
-  num_ary = (1..1_000_000).to_a
-  random_num = num_ary.delete(1 + rand(1_000_000))
+#   num_ary = (1..1_000_000).to_a
+#   random_num = num_ary.delete(1 + rand(1_000_000))
 
-  it "takes and array as an argument and returns a number" do
-    expect(find_duplicate(num_ary).class).to eq 6.class
+#   it "takes and array as an argument and returns a number" do
+#     expect(find_duplicate(num_ary).class).to eq 6.class
+#   end
+
+#   it "returns a number that is not greather than the max value the array" do
+#     expect(find_duplicate(num_ary)).to be <= 1_000_000
+#   end
+
+#   it "returns a number that is not less than the max value the array" do
+#     expect(find_duplicate(num_ary)).to be >= 1
+#   end
+
+#   context "works fine with border values" do
+#     it 'like 1' do
+#       num_ary = (1..1_000_000).to_a
+#       random_num = num_ary.delete(1)
+
+#       expect(find_duplicate(num_ary)).to eq 1
+#     end
+
+#     it "like 1_000_000)" do
+#       num_ary = (1..1_000_000).to_a
+#       random_num = num_ary.delete(1_000_000)
+
+#       expect(find_duplicate(num_ary)).to eq 1_000_000
+#     end
+#   end
+
+#   it "returns the missing number" do
+#     expect(find_duplicate(num_ary)).to eq random_num
+#   end
+# end
+
+
+
+describe "method first_non_repeating" do
+
+  it "return nil for empty string" do
+    expect(first_non_repeating('')).to be_nil
   end
 
-  it "returns a number that is not greather than the max value the array" do
-    expect(find_duplicate(num_ary)).to be <= 1_000_000
+  it "takes and array as an argument and returns a number when a letter is repeated" do
+    char_array = ('a'..'z').to_a.sample(5)
+    duplicate_chars = char_array.sample(3)
+    dup_array = (char_array + duplicate_chars).shuffle.join
+
+    expect(first_non_repeating(dup_array).class).to eq 'a'.class
   end
 
-  it "returns a number that is not less than the max value the array" do
-    expect(find_duplicate(num_ary)).to be >= 1
+  it "returns nil for strings without repeated letters" do 
+    expect(first_non_repeating('aurelio')).to be_nil
   end
 
-  it "works fine with border values (1)" do
-    num_ary = (1..1_000_000).to_a
-    random_num = num_ary.delete(1)
-
-    expect(find_duplicate(num_ary)).to eq 1
+  it "works for strings with only one letter repeated" do 
+    expect(first_non_repeating('Soccer')).to eq "c"
   end
 
-  it "works fine with border values (1_000_000)" do
-    num_ary = (1..1_000_000).to_a
-    random_num = num_ary.delete(1_000_000)
-
-    expect(find_duplicate(num_ary)).to eq 1_000_000
+  it "works for strings with only one non consecutive letter repeated" do 
+    expect(first_non_repeating('cactus')).to eq "c"
   end
 
-  it "returns the missing number" do
-    expect(find_duplicate(num_ary)).to eq random_num
+  it "works with more than one letter repeated" do
+    expect(first_non_repeating('oladead')).to eq "a"
   end
 
+  it "works with capital letter mixed" do
+    expect(first_non_repeating('olAdead')).to eq "a"
+  end
+
+  it "works with repeated letter in the second the last place" do
+    expect(first_non_repeating('Class')).to eq "s"
+  end
 end
