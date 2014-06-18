4a7316b523d793a044b8e0b659ce4978ea5f8da1
Juan Ortiz <juandefelix@yahoo.es>
2014-06-17 21:03:33 -0500

    First and second methos coded and tested

--- lib/bluetechnical.rb
+++ lib/bluetechnical.rb
@@ -1,69 +1,27 @@
-# this one takes always the same amount of time. 
-# The next one takes time depending if the number 
-# is at the beginning or end of the array
-
-# def find_duplicate num_array
-#   sum = (1..1000000).to_a.reduce(:+)
-#   my_sum = num_array.reduce(:+)
-
-#   sum - my_sum
-# end
-
-
 def find_duplicate num_array
   sorted_array = num_array.sort
+  duplicate = nil
 
-  return 1_000_000 if num_array[-1] == 999_999 # special case where the missing 
-                                               # number is the last in the range
+  sorted_array[0..-2].each_with_index do |number, index|
+    remaining = sorted_array[(index+1)..-1]
+    duplicate = number if remaining.include?(number)
+  end
 
-  sorted_array.each_with_index.detect {|e, i| e != i + 1}[0] - 1
+  duplicate
 end
-
-# num_ary = (1..1_000_000).to_a
-# random_num = num_ary.delete(1 + rand(1_000_000))
-
-
-# initial = Time.now
-
-# find_duplicate(num_ary)
-
-# final = Time.now
-
-# puts "Method summing takes: #{final - initial} secs."
-
-
-
-# initial = Time.now
-
-# find_duplicate_order(num_ary)
-
-# final = Time.now
-
-# puts "Method ordering takes: #{final - initial} secs."
-
 
 
 def first_non_repeating string
   sorted_array = string.downcase.chars
 
-  sorted_array[0..-2].each_with_index do |letter, index|
-    remaining = sorted_array[(index+1)..-1]
-    return letter if remaining.include? letter
+  return nil if sorted_array == [] 
+
+  sorted_array.each_with_index do |letter, index|
+    array_copy = sorted_array.dup
+    array_copy.delete_at index
+
+    return letter unless array_copy.include?(letter)
   end
 
-  nil
+  nil 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- spec/bluetechnical_spec.rb
+++ spec/bluetechnical_spec.rb
@@ -1,43 +1,55 @@
 require 'bluetechnical'
 
-# describe "method find_duplicate" do
+# ---------------------- #
+# | find_duplicate tests #
+# ---------------------- #
 
-#   num_ary = (1..1_000_000).to_a
-#   random_num = num_ary.delete(1 + rand(1_000_000))
+describe "method find_duplicate" do
 
-#   it "takes and array as an argument and returns a number" do
-#     expect(find_duplicate(num_ary).class).to eq 6.class
-#   end
+  num_ary = (1..1_000_000).to_a.sample(15)
+  random_num = num_ary.sample
 
-#   it "returns a number that is not greather than the max value the array" do
-#     expect(find_duplicate(num_ary)).to be <= 1_000_000
-#   end
+  num_ary = (num_ary << random_num).shuffle
 
-#   it "returns a number that is not less than the max value the array" do
-#     expect(find_duplicate(num_ary)).to be >= 1
-#   end
+  it "returns nil if there's no repeated numbers" do
+    expect(find_duplicate([1, 2, 3, 4])).to be_nil
+  end
 
-#   context "works fine with border values" do
-#     it 'like 1' do
-#       num_ary = (1..1_000_000).to_a
-#       random_num = num_ary.delete(1)
+  it "takes and array as an argument and returns a number" do
+    expect(find_duplicate(num_ary).class).to eq 6.class
+  end
 
-#       expect(find_duplicate(num_ary)).to eq 1
-#     end
+  it "returns a number that is not greather than the max value the array" do
+    expect(find_duplicate(num_ary)).to be <= 1_000_000
+  end
 
-#     it "like 1_000_000)" do
-#       num_ary = (1..1_000_000).to_a
-#       random_num = num_ary.delete(1_000_000)
+  it "returns a number that is not less than the max value the array" do
+    expect(find_duplicate(num_ary)).to be >= 1
+  end
 
-#       expect(find_duplicate(num_ary)).to eq 1_000_000
-#     end
-#   end
+  context "works fine with border values" do
+    it 'like 1' do
+      num_ary = [1, 1, 3, 6, 34, 67, 23453].shuffle
 
-#   it "returns the missing number" do
-#     expect(find_duplicate(num_ary)).to eq random_num
-#   end
-# end
+      expect(find_duplicate(num_ary)).to eq 1
+    end
 
+    it "like 1_000_000)" do
+      num_ary = [1_000_000, 1_000_000, 2, 3, 4, 234, 2634].shuffle
+
+      expect(find_duplicate(num_ary)).to eq 1_000_000
+    end
+  end
+
+  it "returns the missing number" do
+    expect(find_duplicate(num_ary)).to eq random_num
+  end
+end
+
+
+# --------------------------- #
+# | first_non_repeating tests #
+# --------------------------- #
 
 
 describe "method first_non_repeating" do
@@ -46,35 +58,35 @@ describe "method first_non_repeating" do
     expect(first_non_repeating('')).to be_nil
   end
 
-  it "takes and array as an argument and returns a number when a letter is repeated" do
+  it "takes a string as an argument and returns a letter when a letter is not repeated" do
     char_array = ('a'..'z').to_a.sample(5)
     duplicate_chars = char_array.sample(3)
-    dup_array = (char_array + duplicate_chars).shuffle.join
+    dup_string = (char_array + duplicate_chars).shuffle.join
 
-    expect(first_non_repeating(dup_array).class).to eq 'a'.class
+    expect(first_non_repeating(dup_string).class).to eq 'a'.class
   end
 
-  it "returns nil for strings without repeated letters" do 
-    expect(first_non_repeating('aurelio')).to be_nil
+  it "returns first letter for strings without repeated letters" do 
+    expect(first_non_repeating('aurelio')).to eq 'a'
   end
 
-  it "works for strings with only one letter repeated" do 
-    expect(first_non_repeating('Soccer')).to eq "c"
+  it "works for strings with only one letter non repeated" do 
+    expect(first_non_repeating('oesccero')).to eq "s"
   end
 
-  it "works for strings with only one non consecutive letter repeated" do 
-    expect(first_non_repeating('cactus')).to eq "c"
+  it "returns the first letter non repeated" do
+    expect(first_non_repeating('oladeado')).to eq "l"
   end
 
-  it "works with more than one letter repeated" do
-    expect(first_non_repeating('oladead')).to eq "a"
+  it "doesn't return the second letter non repeated" do
+    expect(first_non_repeating('oladeado')).not_to eq "e"
   end
 
   it "works with capital letter mixed" do
-    expect(first_non_repeating('olAdead')).to eq "a"
+    expect(first_non_repeating('oLAdeado')).to eq "l"
   end
 
   it "works with repeated letter in the second the last place" do
-    expect(first_non_repeating('Class')).to eq "s"
+    expect(first_non_repeating('Clsclas')).to eq "a"
   end
 end
